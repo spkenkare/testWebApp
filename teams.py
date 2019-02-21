47f02619c44227f8761f045b0b03df99efe701c1
Saarila Kenkare <saarilakenkare@gmail.com>
2019-02-21 13:47:33 -0600

    posting/displaying messages works now, need to do subdomains

--- teams.py
+++ teams.py
@@ -9,8 +9,18 @@ from app.db import get_db
 bp = Blueprint('teams', __name__, url_prefix='/teams')
 
 
+@bp.route('/') #at the index
+def displayTeams(): #display all teams
+    teams = []
+    db = get_db()
+    teamQuery = db.execute('SELECT id FROM teams')
+    teamRows = teamQuery.fetchall()
+    for row in teamRows:
+        teams.append(row['id'])
+    return render_template('teams/index.html', teamIDs = teams)
+    
 @bp.route('/', methods=('GET', 'POST'))
-def createTeam():
+def createTeam(): #offer option to create new teams
     teams = []
     db = get_db()
     print(request.form)
@@ -29,6 +39,7 @@ def createTeam():
                 )
                 db.commit()
 
+    """
     elif "message" in request.form:
         if request.method == 'POST':
             teamID = request.form['teamID']
@@ -44,8 +55,8 @@ def createTeam():
                     (message,teamID,)
                 )
                 db.commit()
-
-
+    """
+    """
     teamQuery = db.execute('SELECT id FROM teams')
     teamRows = teamQuery.fetchall()
     for row in teamRows:
@@ -58,10 +69,11 @@ def createTeam():
         teams.append(row['id'])
 
     return render_template('teams/index.html', teamIDs = teams)
+    """
+    return displayTeams()
 
-@bp.route('/messages.html/<teamID>')
-def returnMessages(teamID):
-    #teamID = request.args.get('teamID')
+@bp.route('/messages.html/<teamID>') #for each iframe/team page
+def returnMessages(teamID): #display the current messages
     db = get_db()
     print("Team ID: ", teamID)
     nameQuery = db.execute('SELECT name FROM teams WHERE id = ?', (teamID,))
@@ -74,3 +86,22 @@ def returnMessages(teamID):
     for mRow in messageRows:
         messages.append(mRow['message'])
     return render_template('teams/messages.html', messages = messages, name = name)
+
+@bp.route('/messages.html/<teamID>', methods=('GET', 'POST'))
+def postMessage(teamID):
+    db = get_db()
+    if request.method == 'POST':
+        message = request.form['message']
+        error = None
+
+        if not message:
+            error = 'Message cannot be blank.'
+
+        if error is None:
+            db.execute(
+                'INSERT INTO posts (message, team_id) VALUES (?,?)',
+                (message,teamID,)
+            )
+            db.commit()
+
+    return returnMessages(teamID)
