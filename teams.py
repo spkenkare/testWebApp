b2a817a889e2cb900bc55afe534d388f6ec6c74e
Saarila Kenkare <saarilakenkare@gmail.com>
2019-02-23 09:26:02 -0600

    fixed subdomains

--- teams.py
+++ teams.py
@@ -6,11 +6,12 @@ from flask import (
 
 from app.db import get_db
 
-bp = Blueprint('teams', __name__, url_prefix='/teams')
 
+bp = Blueprint('teams', __name__, url_prefix='/teams')
 
 @bp.route('/') #at the index
 def displayTeams(): #display all teams
+    print("displayTeams")
     teams = []
     db = get_db()
     teamQuery = db.execute('SELECT * FROM teams')
@@ -21,6 +22,7 @@ def displayTeams(): #display all teams
     
 @bp.route('/', methods=('GET', 'POST'))
 def createTeam(): #offer option to create new teams
+    print("createTeam")
     teams = []
     db = get_db()
     print(request.form)
@@ -39,57 +41,39 @@ def createTeam(): #offer option to create new teams
                 )
                 db.commit()
 
-    """
-    elif "message" in request.form:
-        if request.method == 'POST':
-            teamID = request.form['teamID']
-            message = request.form['message']
-            error = None
-
-            if not message:
-                error = 'Team name cannot be blank.'
-
-            if error is None:
-                db.execute(
-                    'INSERT INTO posts (message, team_id) VALUES (?,?)',
-                    (message,teamID,)
-                )
-                db.commit()
-    """
-    """
-    teamQuery = db.execute('SELECT id FROM teams')
-    teamRows = teamQuery.fetchall()
-    for row in teamRows:
-        #messagesQuery = db.execute('SELECT message FROM posts WHERE team_id = ?', (row['id'],))
-        #messageRows = messagesQuery.fetchall()
-        #messages = []
-        #for mRow in messageRows:
-        #    messages.append(mRow['message'])
-        #teams.append((row['id'], row['name'], messages))
-        teams.append(row['id'])
-
-    return render_template('teams/index.html', teamIDs = teams)
-    """
     return displayTeams()
 
-@bp.route('/messages.html/<teamID>', subdomain = '<teamName>') #for each iframe/team page
-def returnMessages(teamID, teamName): #display the current messages
+
+@bp.route('/messages.html', subdomain = '<teamName>') #for each iframe/team page
+def returnMessages(teamName): #display the current messages
+    print("returnMessages") 
     db = get_db()
-    print("Team ID: ", teamID)
-    nameQuery = db.execute('SELECT name FROM teams WHERE id = ?', (teamID,))
-    nameRows = nameQuery.fetchall()
-    name = nameRows[0]['name']
-    print(name)
+    idQuery = db.execute('SELECT id FROM teams WHERE name = ?', (teamName,))
+    idRows = idQuery.fetchall()
+    teamID = idRows[0]['id']
     messagesQuery = db.execute('SELECT message FROM posts WHERE team_id = ?', (teamID,))
     messageRows = messagesQuery.fetchall()
     messages = []
     for mRow in messageRows:
         messages.append(mRow['message'])
-    return render_template('teams/messages.html', messages = messages, name = name)
+    return render_template('teams/messages.html', messages = messages, name = teamName)
 
-@bp.route('/messages.html/<teamID>', methods=('GET', 'POST'))
-def postMessage(teamID): #when someone posts a message to a team
+    #return render_template('teams/messages.html', name = teamName)
+
+
+@bp.route('/messages.html', subdomain = '<teamName>', methods=('GET', 'POST'))
+def postMessage(teamName): #when someone posts a message to a team
+    print("postMessage")
+    #db = get_db()
+    #nameQuery = db.execute('SELECT name FROM teams WHERE id = ?', (teamID,))
+    #nameRows = nameQuery.fetchall()
+    #name = nameRows[0]['name']
+    #return returnMessages(teamID, name)
+
     db = get_db()
+    idQuery = db.execute('SELECT id FROM teams WHERE name = ?', (teamName,))
+    idRows = idQuery.fetchall()
+    teamID = idRows[0]['id']
     if request.method == 'POST':
         message = request.form['message']
         error = None
@@ -104,7 +88,6 @@ def postMessage(teamID): #when someone posts a message to a team
             )
             db.commit()
 
-    nameQuery = db.execute('SELECT name FROM teams WHERE id = ?', (teamID,))
-    nameRows = nameQuery.fetchall()
-    name = nameRows[0]['name']
-    return returnMessages(teamID, name)
+    return returnMessages(teamName)
+
+
