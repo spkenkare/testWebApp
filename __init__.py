9499370e632938b82853f91fa75352bf3f260d40
Saarila Kenkare <saarilakenkare@gmail.com>
2019-02-23 09:26:27 -0600

    added server name to init

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        SERVER_NAME='localhost:5000',
         DATABASE=os.path.join(app.instance_path, 'teams.sqlite'),
     )
 
